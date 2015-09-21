e0fba6b8137f17cbbf3eb4521d2d572b9c5e0fa3
Dominic Cerquetti <binary1230@users.noreply.github.com>
2015-09-21 15:15:17 -0400

    fix copy command

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,7 @@
 git clone https://github.com/magfest/ubersystem-deploy
 
 cd ubersystem-deploy
-cp puppet/fabric_settings.example.ini puppet/fabric_settings.ini
+copy puppet/fabric_settings.example.ini puppet/fabric_settings.ini
 
 ( echo " " & echo. & echo "git_regular_nodes_repo = 'https://github.com/magfest/production-config'" ) >> puppet/fabric_settings.ini
 
