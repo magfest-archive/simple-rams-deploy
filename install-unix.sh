328467660f47110101544c05198091c7d76adb04
Dylan Whichard <dylan@whichard.com>
2016-05-12 22:00:43 -0400

    Fix quotes in vagrant-ssh

--- install-unix.sh
+++ install-unix.sh
@@ -46,7 +46,7 @@ test -d ubersystem-deploy || git clone https://github.com/magfest/ubersystem-dep
 cd ubersystem-deploy
 vagrant up
 cp ../installfiles/run-simple-deploy.sh .
-vagrant ssh -c \'$vm_cmd\'
+vagrant ssh -c "$vm_cmd"
 
 # not sure how / if we should do this on *nix
 # start http://localhost:8000/uber/accounts/insert_test_admin
