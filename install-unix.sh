8eb55dc74ffd8f2b043a928bc3b66c238c95e7a8
Dominic Cerquetti <binary1230@users.noreply.github.com>
2016-05-23 19:17:58 -0400

    fix broken unix deploy
    cherry picked from https://github.com/magfest/simple-rams-deploy/pull/13/files

--- install-unix.sh
+++ install-unix.sh
@@ -46,7 +46,7 @@ git clone https://github.com/magfest/ubersystem-deploy
 cd ubersystem-deploy
 vagrant up
 cp ../installfiles/run-simple-deploy.sh .
-vagrant ssh -c \'$vm_cmd\'
+vagrant ssh -c "$vm_cmd"
 
 # not sure how / if we should do this on *nix
 # start http://localhost:8000/uber/accounts/insert_test_admin
