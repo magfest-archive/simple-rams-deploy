7651e8333103a869ac19eb1cc7d147954a72cbe1
Dominic Cerquetti <dom@magfest.org>
2018-01-22 09:18:46 -0500

    add year-specific event support for deploys

--- installfiles/run-simple-deploy.sh
+++ installfiles/run-simple-deploy.sh
@@ -2,12 +2,13 @@
 set -e
 
 if [[ $# -eq 0 ]] ; then
-  echo "usage: $0 [event_name]"
+  echo "usage: $0 [event_name] [year]"
   exit -1
 fi
 
 EVENT_NAME=$1
+EVENT_YEAR=$2
 
 cd ~/uber/puppet/
 { cat fabric_settings.example.ini; echo -en "\ngit_regular_nodes_repo = 'https://github.com/magfest/production-config'"; } > fabric_settings.ini
-./setup_vagrant_control_server.sh $EVENT_NAME
+./setup_vagrant_control_server.sh $EVENT_NAME $EVENT_YEAR
